f0178bef273ab1fd7316ce572a1bb71839237aa8
cvmgo <design@newlightarts.com>
2025-01-01 12:38:49 -0500

    Data shown from file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-//import { sampleData } from './data';
 import { sharedSubscriptionsData } from './data-shared-subscriptions';
 
 const App = () => {
@@ -26,55 +25,47 @@ const App = () => {
 								<th className='text-left'>Parent Subscription ID</th>
 								<th className='text-left'>Term ID</th>
 								<th className='text-left'>User ID</th>
+								<th className='text-left'>Child Subscriptions</th>
 							</tr>
 						</thead>
 						{/* Table Body */}
 						<tbody>
 							{data.map((item) => (
-								<React.Fragment key={item.subscription_id}>
-									{/* Parent Row */}
-									<tr className='hover:bg-base-50'>
-										<td>{item.subscription_id}</td>
-										<td>{item.term_id}</td>
-										<td>{item.uid}</td>
-									</tr>
-									{/* Conditional Mini Table for Child Subscriptions */}
-									{item.shared_accounts && item.shared_accounts.length > 0 && (
-										<tr>
-											<td colSpan='3' className='bg-base-50'>
-												<div className='flex justify-end'>
-													<div className='w-2/3 p-4'>
-														<h4 className='text-lg font-semibold mb-2'>
-															Child Subscriptions
-														</h4>
-														<table className='w-full border-collapse child-table'>
-															<thead>
-																<tr>
-																	<th className='text-left pb-2'>
-																		Subscription ID
-																	</th>
-																	<th className='text-left pb-2'>
-																		Child User ID
-																	</th>
-																	<th className='text-left pb-2'>Email</th>
-																</tr>
-															</thead>
-															<tbody>
-																{item.shared_accounts.map((child) => (
-																	<tr key={child.account_id}>
-																		<td className='py-1'>{child.account_id}</td>
-																		<td className='py-1'>{child.user_id}</td>
-																		<td className='py-1'>{child.email}</td>
-																	</tr>
-																))}
-															</tbody>
-														</table>
-													</div>
-												</div>
-											</td>
-										</tr>
-									)}
-								</React.Fragment>
+								<tr key={item.subscription_id} className='hover:bg-base-50'>
+									{/* Parent Columns */}
+									<td className='align-top'>{item.subscription_id}</td>
+									<td className='align-top'>{item.term_id}</td>
+									<td className='align-top'>{item.uid}</td>
+									{/* Child Subscriptions Column */}
+									<td>
+										{item.shared_accounts && item.shared_accounts.length > 0 ? (
+											<div className='bg-base-50 p-4'>
+												<table className='w-full border-collapse child-table'>
+													<thead>
+														<tr>
+															<th className='text-left pb-2'>
+																Child Subscription ID
+															</th>
+															<th className='text-left pb-2'>Child User ID</th>
+															<th className='text-left pb-2'>Email</th>
+														</tr>
+													</thead>
+													<tbody>
+														{item.shared_accounts.map((child) => (
+															<tr key={child.account_id}>
+																<td className='py-1'>{child.account_id}</td>
+																<td className='py-1'>{child.user_id}</td>
+																<td className='py-1'>{child.email}</td>
+															</tr>
+														))}
+													</tbody>
+												</table>
+											</div>
+										) : (
+											<span>No child subscriptions</span>
+										)}
+									</td>
+								</tr>
 							))}
 						</tbody>
 					</table>
